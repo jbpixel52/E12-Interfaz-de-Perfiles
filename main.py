4eff6dc620eda7542044446b5cd9ddcc3bc30465
jbpixel52 <50649423+jbpixel52@users.noreply.github.com>
2020-05-01 16:40:29 -0700

    listas de registro

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import os
 from tkinter import *
 from PIL import Image
 from tkinter import messagebox
+from logica_registro import *
 class App:
     def __init__(self,master):
             frame = Frame(master)
             frame.pack()
-            self.button= Button(frame,text='X',fg='blue',command = frame.quit())
-            self.button.pack(side=BOTTOM)
 
-            self.easter_egg = Button(frame,text='JIJI',fg='pink',command = self.func_egg)
-            self.easter_egg.pack(side= TOP)
     def func_egg(self):
         messagebox.showinfo('Easter Egg','EL JAJAS ESTUVO AQUI XD')
+    def register_window(self):
+        main_frame = Frame()
+
 
 root= Tk()
 
