81d669524a6cf1b97cb3c66912e787fcbe49c88b
jbpixel52 <50649423+jbpixel52@users.noreply.github.com>
2020-05-01 17:29:29 -0700

    agregue titulos

--- main.py
+++ main.py
@@ -5,16 +5,18 @@ from tkinter import messagebox
 from logica_registro import *
 class App:
     def __init__(self,master):
-        self.frame = Frame(master, width = 160, height = 90)
+        self.frame = Frame(master, width = 1600, height = 900, bg='red')
         self.frame.pack()
-
 
         #self.register_window()
     def initial_window(self):
-        top_frame = Frame(self.frame)
-        top_frame.pack()
-        titulo = Label(top_frame,text='Alumnos')
-        titulo.pack()
+        top_frame = Frame(self.frame, bg='red')
+        top_frame.pack(side=TOP)
+        titulo = Label(top_frame,text='Alumnos', width=X, height=Y)
+        titulo.pack(side=LEFT)
+
+        quit_button = Button(top_frame, text = 'X',bg='red',command= self.frame.quit())
+        quit_button.pack()
     def func_egg(self):
        return  messagebox.showinfo('Easter Egg','EL JAJAS ESTUVO AQUI XD')
 
