f296c3073598309f08171576507866da99e11f42
jbpixel52 <50649423+jbpixel52@users.noreply.github.com>
2020-05-01 20:58:56 -0700

    prueba xd

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PIL import Image, ImageGrab
 from tkinter import messagebox
 from logica_registro import *
 
-
+print('pendejo el q lo leA')
 root = Tk()
 #root.overrideredirect(True) # removes title bar
 
