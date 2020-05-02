6c235f8557f81c0951609c1cd26c3ba8bd8f68ab
jbpixel52 <50649423+jbpixel52@users.noreply.github.com>
2020-05-01 17:15:22 -0700

    ...

--- main.py
+++ main.py
@@ -5,17 +5,33 @@ from tkinter import messagebox
 from logica_registro import *
 class App:
     def __init__(self,master):
-            frame = Frame(master)
-            frame.pack()
+        self.frame = Frame(master, width = 160, height = 90)
+        self.frame.pack()
 
+
+        #self.register_window()
+    def initial_window(self):
+        top_frame = Frame(self.frame)
+        top_frame.pack()
+        titulo = Label(top_frame,text='Alumnos')
+        titulo.pack()
     def func_egg(self):
-        messagebox.showinfo('Easter Egg','EL JAJAS ESTUVO AQUI XD')
+       return  messagebox.showinfo('Easter Egg','EL JAJAS ESTUVO AQUI XD')
+
+    def fetch_text(self,arg):#funcion para leer el texto de los entry boxes de nombre
+        return arg.get()
     def register_window(self):
-        main_frame = Frame()
+
+        input_nombre =  Entry()
+        input_nombre.pack()
+        confirm_button = Button(state=DISABLED, Text='Haz click para confirmar nombre', command = self.func_egg())
+        confirm_button.pack()
 
 
 root= Tk()
 
 app = App(root)
+app.initial_window()
+
 
 mainloop()
