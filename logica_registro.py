6f9543a66cf8623408f11e94cca472994d3a0d1e
jbpixel52 <50649423+jbpixel52@users.noreply.github.com>
2020-05-01 21:54:01 -0700

    agregue docstrings, son descripciones de cada funcion

--- logica_registro.py
+++ logica_registro.py
@@ -1,17 +1,19 @@
 from json import *
 
-dias=[]
-meses=[]
-anios=[]
-user_inputs ={'Nombre','Apellido Paterno','Apellido Materno','Dia','Mes','Anio','Descripcion'}
+dias = []
+meses = []
+anios = []
+user_inputs = {'Nombre', 'Apellido Paterno',
+               'Apellido Materno', 'Dia', 'Mes', 'Anio', 'Descripcion'}
+
 
 def fill_lists():
     for dia in range(31):
         dias.append(dia)
     for mes in range(12):
         meses.append(mes)
-    for anio in range(1990,2020):
+    for anio in range(1990, 2020):
         anios.append(anio)
+
+
 fill_lists()
-
-
--- main.py
+++ main.py
@@ -23,7 +23,10 @@ get_screen_size()
 
 
 class App:
+    """CLASE donde se inicializa el programa."""
+
     def __init__(self, master):
+        """INICIALIZACION DEL APP."""
         self.frame = Frame(master, width=1600, height=900, bg="red")
         self.frame.pack(fill=X, expand=True)
         self.background = Canvas(master, bg='blue')
@@ -32,22 +35,34 @@ class App:
         # self.register_window()
 
     def initial_window(self):
+        """INICIALIZACION DEL MAIN VIEW O INITIAL VIEW."""
         top_frame = Frame(self.frame, bg='green')
         top_frame.pack(side=TOP, fill=X, expand=True)
         titulo = Label(top_frame, text='Alumnos', bg='green')
         titulo.pack(side=LEFT)
 
         quit_button = Button(top_frame, text='X', bg='red',
-                             command=root.destroy)
+                             command=root.destroy)  # ESTE BOTON DESTRUYE/CIERRA EL PROGRAMA
         quit_button.pack(side=RIGHT)
         confirm_button = Button(
-            state=DISABLED, Text='Haz click para confirmar nombre', command=self.func_egg())
+            state=DISABLED, Text='Haz click para confirmar nombre',
+            command=self.func_egg())
         confirm_button.pack(side=RIGHT)
 
     def func_egg(self):
+        """ PEQUENO EASTER EGG DE PRUEBA."""
         return messagebox.showinfo('Easter Egg', 'EL JAJAS ESTUVO AQUI XD')
 
-    def fetch_text(self, arg):  # funcion para leer el texto de los entry boxes de nombre
+    def fetch_text(self, arg):
+        """
+
+        Funcion para leer el texto de los entry boxes de nombre.
+
+        PARAMETRO:
+        arg: este argumento es cualquier Entry widget
+        regresa lo que este dentro.
+
+        """
         return arg.get()
 
 
